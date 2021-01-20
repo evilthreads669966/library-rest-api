f2a6071665c52c017c884a338803942f0eba2325
Chris Basinger <evilthreads669966@gmail.com>
2021-01-20 14:10:22 -0600

    - use FunSpec for testing routes

---
+++ test/KoinTest.kt
@@ -0,0 +1,4 @@
+package com.chrisbasinger
+
+class KoinTest {
+}
--- test/RouteTest.kt
+++ test/RouteTest.kt
@@ -5,16 +5,19 @@ import com.google.gson.Gson
 import io.kotest.assertions.ktor.shouldHaveContent
 import io.kotest.assertions.ktor.shouldHaveStatus
 import io.kotest.assertions.ktor.shouldNotHaveStatus
+import io.kotest.core.script.describe
+import io.kotest.core.spec.style.FunSpec
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.Test
 
 /*this test won't run and I can't figure it out. It's not failing it just won't get picked up by the ide*/
 
-class RouteTest {
-    @Test
-    fun testRoot() {
-        val libraries = setOf(
+class RouteTest: FunSpec({
+    lateinit var libraries: Collection<AndroidLibrary>
+    lateinit var gson: Gson
+
+    beforeTest {
+        libraries = setOf(
             AndroidLibrary(0, "EvadeMe", "evilthreads669966", "https://github.com/evilthreads669966/evademe", "A Kotlin heuristics evasion library for Android."),
             AndroidLibrary(1, "Pickpocket", "evilthreads669966", "https://github.com/evilthreads669966/pickpocket", "A Kotlin library for Android with content provider queries and support for reactive streams and coroutines."),
             AndroidLibrary(2, "Bootlaces", "evilthreads669966", "https://github.com/evilthreads669966/bootlaces", "A Kotlin work manager library for Android with support for progress notifications and Hilt support."),
@@ -22,26 +25,34 @@ class RouteTest {
             AndroidLibrary(4, "Jackhammer", "evilthreads669966", "https://github.com/evilthreads669966/jackhammer", "A Kotlin brute force privilege escalation library for Android that wraps your code in a brute force permission request."),
             AndroidLibrary(5, "SmsBackdoor", "evilthreads669966", "https://github.com/evilthreads669966/smsbackdoor", " A Kotlin Android library that opens a persistent binary SMS backdoor with the ability to define your own remote command handler."),
         )
-        val gson = Gson()
+        gson = Gson()
+    }
 
+    test("Should return proper response codes and content for routes"){
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/api/libraries").apply {
-                response shouldHaveContent gson.toJson(libraries)
-                response shouldNotHaveStatus HttpStatusCode.NoContent
-                response shouldHaveStatus HttpStatusCode.OK
+            describe("Should return libraries in json format and a 200 response code"){
+                handleRequest(HttpMethod.Get, "/api/libraries").run {
+                    response shouldHaveContent gson.toJson(libraries)
+                    response shouldNotHaveStatus HttpStatusCode.NoContent
+                    response shouldHaveStatus HttpStatusCode.OK
+                }
             }
-            handleRequest { uri = "/api/libraries/0" }.run {
-                response shouldHaveContent gson.toJson(libraries.find { it.id == 0 })
-                response shouldNotHaveStatus HttpStatusCode.BadRequest
-                response shouldNotHaveStatus HttpStatusCode.NotFound
-                response shouldHaveStatus HttpStatusCode.OK
+            describe("Should return first library in collection in json format and a 200 response code"){
+                handleRequest { uri = "/api/libraries/0" }.run {
+                    response shouldHaveContent gson.toJson(libraries.find { it.id == 0 })
+                    response shouldNotHaveStatus HttpStatusCode.BadRequest
+                    response shouldNotHaveStatus HttpStatusCode.NotFound
+                    response shouldHaveStatus HttpStatusCode.OK
+                }
             }
-            handleRequest { uri = "/api/libraries/100" }.run {
-                response shouldHaveContent "Library not found"
-                response shouldNotHaveStatus HttpStatusCode.BadRequest
-                response shouldNotHaveStatus  HttpStatusCode.OK
-                response shouldHaveStatus  HttpStatusCode.NotFound
+            describe("Should return library not found and 404 response code when parameter is larger that collection size"){
+                handleRequest { uri = "/api/libraries/100" }.run {
+                    response shouldHaveContent "Library not found"
+                    response shouldNotHaveStatus HttpStatusCode.BadRequest
+                    response shouldNotHaveStatus  HttpStatusCode.OK
+                    response shouldHaveStatus  HttpStatusCode.NotFound
+                }
             }
         }
     }
-}
+})

dc65ad86f6e3c5caaebf83cf04bf4a229998e3aa
Chris Basinger <evilthreads669966@gmail.com>
2021-01-20 13:47:19 -0600

    - shadow library - annotation for experimental locations api

--- src/Application.kt
+++ src/Application.kt
@@ -17,6 +17,7 @@ import org.slf4j.event.Level
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+@KtorExperimentalLocationsAPI
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
--- src/Routes.kt
+++ src/Routes.kt
@@ -4,6 +4,7 @@ import io.ktor.locations.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
+@KtorExperimentalLocationsAPI
 fun <T: Item> Route.libraryRoutes(service: IService<T>){
     get<Libraries> {
         if(!service.hasItems())
@@ -11,9 +12,9 @@ fun <T: Item> Route.libraryRoutes(service: IService<T>){
         else
             call.respond(service.getAllItems()!!)
     }
-    get<Library> {
-        val id = it.id ?: call.respond(HttpStatusCode.BadRequest, "No id error")
-        val library = service.getItem(id as Int) ?: call.respond(HttpStatusCode.NotFound, "Library not found")
+    get<Library> { library ->
+        library.id ?: call.respond(HttpStatusCode.BadRequest, "No id error")
+        val library = service.getItem(library.id as Int)!! ?: call.respond(HttpStatusCode.NotFound, "Library not found")
         call.respond(library)
     }
 }
